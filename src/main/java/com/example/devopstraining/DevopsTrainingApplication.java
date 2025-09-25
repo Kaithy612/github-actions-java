5f1dd53dc1355464dc5b90d3cb659d2e61cbb953
Kaithyn <kaithlyn.vaals@hotmail.com>
2025-09-25 14:54:09 +0200

    Changed to final

--- src/main/java/com/example/devopstraining/DevopsTrainingApplication.java
+++ src/main/java/com/example/devopstraining/DevopsTrainingApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DevopsTrainingApplication {
+public final class DevopsTrainingApplication {
 
      private DevopsTrainingApplication() {}
 
