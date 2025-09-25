41a325763c620ec95111607c1516ede37c4c830d
Kaithyn <kaithlyn.vaals@hotmail.com>
2025-09-25 15:11:25 +0200

    Final

--- src/main/java/com/example/devopstraining/controller/HelloController.java
+++ src/main/java/com/example/devopstraining/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-public class HelloController {
+public final class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
